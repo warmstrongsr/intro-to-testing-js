35d444cf7318dcc59f562559305e600a33978f7a
Will Armstrong <will.ea.armstrong@gmail.com>
2022-08-10 12:14:44 -0500

    Update tests.js

--- tests.js
+++ tests.js
@@ -13,3 +13,20 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe ('hellrld', function () {
+    it('should be a defined function', function(){
+        expect(typeof helloWorld).toBe('function');
+
+        });
+    it('should return a string when called', function(){
+        expect(typeof helloWorld()).toBe("string");
+    });
+    it('should return the string "Hello, World" when executed', function() {
+        expect(helloWorld()).toBe("Hello, World!");
+    });
+    it("should never return 'undefined' when called", function (){
+        expect(helloWorld()).not.toBe(undefined);
+    });
+
+    });
